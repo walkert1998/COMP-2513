b5841f95381c0a3dc5e7ef3ad41a5e56d48a6e8a
walkert1998 <twalker.mhd@gmail.com>
2020-10-17 21:01:05 -0300

    started doing jquery stuff

--- js/script.js
+++ js/script.js
@@ -1,20 +1,57 @@
+/*
+ * Licensed Funeral Homes, Crematoriums Cemeteries and Funeral Sellers in NS
+ * https://data.novascotia.ca/Permits-and-Licensing/Licensed-Funeral-Homes-Crematoriums-Cemeteries-and/if4h-78fy
+ *
+ */
+
 $(document).ready(function() {
-	$("#getStudentsButton").click(getStudents);
+	$("#getDataButton").click(getData);
 });
 
+function getCities() {
+	let dropdown = $('#city_selection');
+	dropdown.empty();
+	// dropdown.append('<option selected="true" disabled>Choose State/Province</option>');
+	// dropdown.prop('selectedIndex', 0);
+	console.log("fetching data...");
+	var url = 'https://data.novascotia.ca/resource/if4h-78fy.json';
+    $.getJSON(url, function (data) {
+		dataUnique = jQuery.uniqueSort(data);
+		$.each(dataUnique, function (key, entry) {
+		
+		  dropdown.append($('<option></option>').val(entry.city).html(entry.city));
+		  console.log(entry.city);
+		})
+	});
+}
 
-function getStudents() {
-	console.log("run stuff!");
-	var url = 'http://comp2513.azurewebsites.net/api/students';
+function getData() {
+
+	console.log("fetching data...");
+	var url = 'https://data.novascotia.ca/resource/if4h-78fy.json';
 
 	$.ajax({
 		url: url,
 		method: 'get'
 	}).done(function(data) {
-		//console.log(data);
+		
 		for(var i = 0; i < data.length; i++) {
-			console.log("FirstName: " + data[i].FirstName + ", LastName: " + data[i].LastName);
-
+			if (data[i].city.indexOf('DARTMOUTH') === 0) {
+				console.log("License Type: " + data[i].license_type);
+			}
 		}
+		
+		/*
+		$.each(data, function(index, element) {
+			if (element.geography.indexOf('Barrington') === 0 &&
+				element.violations.indexOf('motor vehicle') !== -1) {
+				console.log("Entry:___________________________");
+				$.each(element, function(propName, propVal) {
+	  				console.log(propName, propVal);
+				});
+			}
+		});
+		*/
 	});
+
 }
