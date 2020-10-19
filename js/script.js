c29cf7cfc36486d146cf6fe830cf3e71ca7ba933
walkert1998 <twalker.mhd@gmail.com>
2020-10-19 00:31:53 -0300

    more work done

--- js/script.js
+++ js/script.js
@@ -4,9 +4,22 @@
  *
  */
 
+var retrievedData;
+
 $(document).ready(function() {
 	$("#getDataButton").click(getData);
 });
+
+
+
+function getAllData() {
+	console.log("fetching data...");
+	var url = 'https://data.novascotia.ca/resource/if4h-78fy.json';
+    $.getJSON(url, function (data) {
+		retrievedData = data;
+	});
+	getCities();
+}
 
 function getCities() {
 	let dropdown = $('#city_selection');
@@ -15,21 +28,21 @@ function getCities() {
 	// dropdown.prop('selectedIndex', 0);
 	console.log("fetching data...");
 	var url = 'https://data.novascotia.ca/resource/if4h-78fy.json';
-    $.getJSON(url, function (data) {
-		var cities = new Array;
-		$.each(data, function (key, entry) {
-		  if (jQuery.inArray(entry.city, cities) === -1) {
+	var cities = new Array;
+	$.each(retrievedData, function (key, entry) {
+		console.log(retrievedData[0]);
+		if (jQuery.inArray(entry.city, cities) === -1) {
 			cities.push(entry.city);
-		  }
-		  else {
+		}
+		else {
 			console.log("City " + entry.city + " already loaded");
-		  }
-		})
-		cities.sort();
-		console.log(cities.length + " cities retrieved.");
-		$.each(cities, function (index, value) {
-			dropdown.append($('<option></option>').val(cities[index]).html(cities[index]));
-		})
+		}
+	})
+	cities.sort();
+	console.log(cities.length + " cities retrieved.");
+	dropdown.append($('<option></option>').val("Any").html("Any"));
+	$.each(cities, function (index, value) {
+		dropdown.append($('<option></option>').val(cities[index]).html(cities[index]));
 	});
 }
 
@@ -40,6 +53,8 @@ function getData() {
 	var city_selection = $('#city_selection').val();
 	var license_selection = $('#license_selection').val();
 	var results = new Array;
+	let result_section = $('#results');
+	result_section.empty();
 	$.ajax({
 		url: url,
 		method: 'get'
@@ -47,15 +62,37 @@ function getData() {
 		console.log(city_selection);
 		console.log(license_selection);
 		for(var i = 0; i < data.length; i++) {
-			if (data[i].city.indexOf(city_selection) === 0 && data[i].license_type.indexOf(license_selection) === 0) {
+			if (city_selection !== "Any" && license_selection !== "Any") {
+				if (data[i].city.indexOf(city_selection) === 0 && data[i].license_type.indexOf(license_selection) === 0) {
+					console.log(data[i]);
+					results.push(data[i]);
+				}
+			} else if (city_selection != "Any") {
+				console.log(city_selection + ' ' + license_selection);
+				if (data[i].city.indexOf(city_selection) === 0) {
+					console.log(data[i]);
+					results.push(data[i]);
+				}
+			} else if (license_selection != "Any") {
+				if (data[i].license_type.indexOf(license_selection) === 0) {
+					console.log(data[i]);
+					results.push(data[i]);
+				}
+			} else {
 				console.log(data[i]);
 				results.push(data[i]);
 			}
 		}
 		if (results.length < 1) {
 			console.log("No results");
+			result_section.append($('<h2></h2>').html('No Results Found...'));
+		} else {
+			result_section.append($('<h2></h2>').html(results.length + ' Results Found:'));
+			result_section.append($('<div class="row result"></div>').html('<div class="col-sm-4"><p>Name</p></div><div class="col-sm-4"><p>Services</p></div><div class="col-sm-4"><p>Location</p></div>'));
+			$.each(results, function (index, value) {
+				result_section.append($('<a href="#"></a>').html('<div class="row result"><div class="col-sm-4"><p>'+ results[index].licensee_name + '</p></div><div class="col-sm-4"><p>'+ results[index].license_type + '</p></div><div class="col-sm-4"><p>'+ results[index].city + '</p></div></div><br  />'));
+			})
 		}
-		
 		/*
 		$.each(data, function(index, element) {
 			if (element.geography.indexOf('Barrington') === 0 &&
