44c9ef840a43011e4f2fa1cf29e6f3665c9cbb9e
walkert1998 <twalker.mhd@gmail.com>
2020-10-19 00:47:50 -0300

    started making actual functionality

--- js/script.js
+++ js/script.js
@@ -6,22 +6,17 @@
 
 var retrievedData;
 
-$(document).ready(function() {
-	$("#getDataButton").click(getData);
-});
-
-
-
 function getAllData() {
 	console.log("fetching data...");
 	var url = 'https://data.novascotia.ca/resource/if4h-78fy.json';
     $.getJSON(url, function (data) {
 		retrievedData = data;
+		getCities(retrievedData);
+		$("#getDataButton").attr('onclick', 'getData(retrievedData)');
 	});
-	getCities();
 }
 
-function getCities() {
+function getCities(data) {
 	let dropdown = $('#city_selection');
 	dropdown.empty();
 	// dropdown.append('<option selected="true" disabled>Choose State/Province</option>');
@@ -29,7 +24,7 @@ function getCities() {
 	console.log("fetching data...");
 	var url = 'https://data.novascotia.ca/resource/if4h-78fy.json';
 	var cities = new Array;
-	$.each(retrievedData, function (key, entry) {
+	$.each(data, function (key, entry) {
 		console.log(retrievedData[0]);
 		if (jQuery.inArray(entry.city, cities) === -1) {
 			cities.push(entry.city);
@@ -46,8 +41,7 @@ function getCities() {
 	});
 }
 
-function getData() {
-
+function getData(data) {
 	console.log("fetching data...");
 	var url = 'https://data.novascotia.ca/resource/if4h-78fy.json';
 	var city_selection = $('#city_selection').val();
@@ -55,55 +49,40 @@ function getData() {
 	var results = new Array;
 	let result_section = $('#results');
 	result_section.empty();
-	$.ajax({
-		url: url,
-		method: 'get'
-	}).done(function(data) {
-		console.log(city_selection);
-		console.log(license_selection);
-		for(var i = 0; i < data.length; i++) {
-			if (city_selection !== "Any" && license_selection !== "Any") {
-				if (data[i].city.indexOf(city_selection) === 0 && data[i].license_type.indexOf(license_selection) === 0) {
-					console.log(data[i]);
-					results.push(data[i]);
-				}
-			} else if (city_selection != "Any") {
-				console.log(city_selection + ' ' + license_selection);
-				if (data[i].city.indexOf(city_selection) === 0) {
-					console.log(data[i]);
-					results.push(data[i]);
-				}
-			} else if (license_selection != "Any") {
-				if (data[i].license_type.indexOf(license_selection) === 0) {
-					console.log(data[i]);
-					results.push(data[i]);
-				}
-			} else {
+	for(var i = 0; i < data.length; i++) {
+		if (city_selection !== "Any" && license_selection !== "Any") {
+			if (data[i].city.indexOf(city_selection) === 0 && data[i].license_type.indexOf(license_selection) === 0) {
 				console.log(data[i]);
 				results.push(data[i]);
 			}
-		}
-		if (results.length < 1) {
-			console.log("No results");
-			result_section.append($('<h2></h2>').html('No Results Found...'));
-		} else {
-			result_section.append($('<h2></h2>').html(results.length + ' Results Found:'));
-			result_section.append($('<div class="row result"></div>').html('<div class="col-sm-4"><p>Name</p></div><div class="col-sm-4"><p>Services</p></div><div class="col-sm-4"><p>Location</p></div>'));
-			$.each(results, function (index, value) {
-				result_section.append($('<a href="#"></a>').html('<div class="row result"><div class="col-sm-4"><p>'+ results[index].licensee_name + '</p></div><div class="col-sm-4"><p>'+ results[index].license_type + '</p></div><div class="col-sm-4"><p>'+ results[index].city + '</p></div></div><br  />'));
-			})
-		}
-		/*
-		$.each(data, function(index, element) {
-			if (element.geography.indexOf('Barrington') === 0 &&
-				element.violations.indexOf('motor vehicle') !== -1) {
-				console.log("Entry:___________________________");
-				$.each(element, function(propName, propVal) {
-	  				console.log(propName, propVal);
-				});
+		} else if (city_selection != "Any") {
+			console.log(city_selection + ' ' + license_selection);
+			if (data[i].city.indexOf(city_selection) === 0) {
+				console.log(data[i]);
+				results.push(data[i]);
 			}
-		});
-		*/
-	});
+		} else if (license_selection != "Any") {
+			if (data[i].license_type.indexOf(license_selection) === 0) {
+				console.log(data[i]);
+				results.push(data[i]);
+			}
+		} else {
+			console.log(data[i]);
+			results.push(data[i]);
+		}
+	}
+	if (results.length < 1) {
+		console.log("No results");
+		result_section.append($('<h2></h2>').html('No Results Found...'));
+	} else {
+		result_section.append($('<h2></h2>').html(results.length + ' Results Found:'));
+		result_section.append($('<div class="row result"></div>').html('<div class="col-sm-4"><p>Name</p></div><div class="col-sm-4"><p>Services</p></div><div class="col-sm-4"><p>Location</p></div>'));
+		$.each(results, function (index, value) {
+			result_section.append($('<a href="#"></a>').html('<div class="row result"><div class="col-sm-4"><p>'+ results[index].licensee_name + '</p></div><div class="col-sm-4"><p>'+ results[index].license_type + '</p></div><div class="col-sm-4"><p>'+ results[index].city + '</p></div></div><br  />'));
+		})
+	}
+}
 
+function displayEstablishment () {
+	$('#establishment-name')
 }
