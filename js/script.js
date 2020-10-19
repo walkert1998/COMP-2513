b14912eb25ac2d8bc7316bc418cdc995010b0748
walkert1998 <twalker.mhd@gmail.com>
2020-10-18 23:06:57 -0300

    added some styling and google maps

--- js/script.js
+++ js/script.js
@@ -16,11 +16,19 @@ function getCities() {
 	console.log("fetching data...");
 	var url = 'https://data.novascotia.ca/resource/if4h-78fy.json';
     $.getJSON(url, function (data) {
-		dataUnique = jQuery.uniqueSort(data);
-		$.each(dataUnique, function (key, entry) {
-		
-		  dropdown.append($('<option></option>').val(entry.city).html(entry.city));
-		  console.log(entry.city);
+		var cities = new Array;
+		$.each(data, function (key, entry) {
+		  if (jQuery.inArray(entry.city, cities) === -1) {
+			cities.push(entry.city);
+		  }
+		  else {
+			console.log("City " + entry.city + " already loaded");
+		  }
+		})
+		cities.sort();
+		console.log(cities.length + " cities retrieved.");
+		$.each(cities, function (index, value) {
+			dropdown.append($('<option></option>').val(cities[index]).html(cities[index]));
 		})
 	});
 }
@@ -29,16 +37,23 @@ function getData() {
 
 	console.log("fetching data...");
 	var url = 'https://data.novascotia.ca/resource/if4h-78fy.json';
-
+	var city_selection = $('#city_selection').val();
+	var license_selection = $('#license_selection').val();
+	var results = new Array;
 	$.ajax({
 		url: url,
 		method: 'get'
 	}).done(function(data) {
-		
+		console.log(city_selection);
+		console.log(license_selection);
 		for(var i = 0; i < data.length; i++) {
-			if (data[i].city.indexOf('DARTMOUTH') === 0) {
-				console.log("License Type: " + data[i].license_type);
+			if (data[i].city.indexOf(city_selection) === 0 && data[i].license_type.indexOf(license_selection) === 0) {
+				console.log(data[i]);
+				results.push(data[i]);
 			}
+		}
+		if (results.length < 1) {
+			console.log("No results");
 		}
 		
 		/*
