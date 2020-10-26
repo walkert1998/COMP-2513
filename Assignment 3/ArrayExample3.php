4a0ab6d9145bd9f2603f6b0221b947fe3534e60e
walkert1998 <twalker.mhd@gmail.com>
2020-10-26 16:02:07 -0300

    Added array

--- Assignment 3/ArrayExample3.php
+++ Assignment 3/ArrayExample3.php
@@ -2,6 +2,12 @@
 date_default_timezone_set("America/Halifax");
 echo "Author: Thomas Walker" . "<br>";
 echo "Date: " . date("d-m-Y h:i:sa") . "<br>";
+$cars = array (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+);
 for ($row = 0; $row < 4; $row++) {
   echo "<p><b>Row number $row</b></p>";
   echo "<ul>";
