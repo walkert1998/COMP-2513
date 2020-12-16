19c310c41d95c2d618e5dba89142ca3ae47372d0
walkert1998 <twalker.mhd@gmail.com>
2020-12-15 21:31:59 -0400

    started final project

---
+++ Final Project/mevn-app/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="row">
+      <a @click="toggleTheme">Toggle Theme</a>
+    </div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/bookList">Book list</router-link> |
+      <router-link to="/weather">Weather</router-link> |
+      <router-link to="/map">Map</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleTheme() {
+      document.body.classList.toggle('dark');
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: var(--color-page-background);
+  color: var(--color-text);
+}
+
+body {
+  min-height: 100vh;
+  margin: 0;
+  height: 100%;
+  background-color: var(--color-page-background);
+}
+
+#nav {
+  padding: 30px;
+  padding-bottom: 50px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.row {
+  position: absolute;
+  right: 10%;
+  top: 1%;
+}
+</style>
---
+++ Final Project/mevn-app/src/views/Map.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="map">
+    <h3>Search for public housing</h3>
+    <div>
+        <p>nothing</p>
+    </div>
+    <div>
+        <input type="text" id="address-search"/>
+    </div>
+  </div>
+</template>
