d1c5cd52fd7a8ca528f3b510418ade8e7dce9410
walkert1998 <twalker.mhd@gmail.com>
2020-12-16 21:49:55 -0400

    adjusted columns

--- Final Project/mevn-app/src/views/Map.vue
+++ Final Project/mevn-app/src/views/Map.vue
@@ -1,22 +1,33 @@
 <template>
   <div class="map">
-      <div class="row justify-content-center">
-        <div class="col-s-12">
-          <h3>Search for public housing</h3>
-        </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-s-4">
-          <p>none</p>
+    <div class="row">
+      <div class="col-s-12 col-md-4">
+        <div class="row">
+          <div class="col-s-12 col-md-12">
+            <h4>Search for public housing</h4>
+          </div>
+          <div class="col-s-12 col-md-12">
+            <input type="text" id="address-search"/>
+            <button>Search</button>
+          </div>
+          <div class="col-s-12 col-md-12">
+            <p>none</p>
             <div v-for="unit in housingunits" :key="unit._id">
               <h3>{{unit.name}}</h3>
               <p>{{unit.residential_units}}</p>
               <p>{{unit.civic_address}}</p>
             </div>
-        </div>
-        <div class="col-s-8">
-            <input type="text" id="address-search"/>
+          </div>
         </div>
       </div>
+      <div class="col-s-12 col-md-8">
+
+      </div>
     </div>
+    
+    <div class="row justify-content-center">
+      
+      
+    </div>
+  </div>
 </template>
