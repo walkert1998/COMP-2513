2b6a7c6a772f50ea37d373b92308c39fe92689a3
walkert1998 <twalker.mhd@gmail.com>
2020-12-18 21:53:52 -0400

    updated home page

--- Final Project/mevn-app/src/views/Home.vue
+++ Final Project/mevn-app/src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-    <h3>Welcome</h3>
-    <p>Visit the book list.</p>
+    <h1>Welcome</h1>
+    <p>This site is intended to help you locate public housing facilities for seniors in Nova Scotia.</p>
+    <p>Data taken from <a target="_blank" href="https://data.novascotia.ca/Community-Services/Public-Housing-Units-Nova-Scotia-Seniors/2d4m-9e6x">data.novascotia.ca</a></p>
+    <p>To begin searching, <router-link to="/map">visit the Map now!</router-link>.</p>
+    <h3>Features</h3>
+    <p>You can toggle between "light" and "dark" modes by selecting the "Toggle Theme" button in the bottom left corner of the screen.</p>
   </div>
 </template>
 
