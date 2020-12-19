c79973c3677d54c28771ad8fe52b1c4b822beda3
walkert1998 <twalker.mhd@gmail.com>
2020-12-18 23:39:19 -0400

    updated home page

--- Final Project/mevn-app/src/views/Home.vue
+++ Final Project/mevn-app/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Welcome</h1>
+    <h3>Welcome</h3>
     <p>This site is intended to help you locate public housing facilities for seniors in Nova Scotia.</p>
     <p>Data taken from <a target="_blank" href="https://data.novascotia.ca/Community-Services/Public-Housing-Units-Nova-Scotia-Seniors/2d4m-9e6x">data.novascotia.ca</a></p>
     <p>To begin searching, <router-link to="/map">visit the Map now!</router-link>.</p>
